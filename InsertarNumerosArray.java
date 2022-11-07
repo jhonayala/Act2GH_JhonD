f1e201997ac2e48ba21420b8204ae817f68a47e2
jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2022-11-07 14:44:03 +0100

    metodo para mostrar el contenido del array por pantalla

--- InsertarNumerosArray.java
+++ InsertarNumerosArray.java
@@ -2,13 +2,16 @@ public class InsertarNumerosArray {
     public static void main(String[] args) {
         int[] array = new int[20];
         InsertarNumeros(array);
-        for (int i = 0; i < array.length; i++)
-            System.out.print("[" + array[i] + "] ");
+        MostrarArray(array);
     }
 
     public static void InsertarNumeros(int[] array) {
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * (10 + 1));
         }
+    }
+    public static void MostrarArray(int [] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print("[" + array[i] + "] ");
     }
 }
