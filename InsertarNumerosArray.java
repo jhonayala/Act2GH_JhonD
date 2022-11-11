b6a71e617ad61635959233f5b91356ab47df9a5d
jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2022-11-11 10:45:08 +0100

    Metodo para contar el numero de veces que se repite un numero

--- InsertarNumerosArray.java
+++ InsertarNumerosArray.java
@@ -2,13 +2,37 @@ public class InsertarNumerosArray {
     public static void main(String[] args) {
         int[] array = new int[20];
         InsertarNumeros(array);
-        for (int i = 0; i < array.length; i++)
-            System.out.print("[" + array[i] + "] ");
+        MostrarArray(array);
+        ContarRepeticiones(array);
+
     }
 
     public static void InsertarNumeros(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * (10 + 1));
+            array[i] = (int) (Math.random() * 10 + 1);
+        }
+    }
+
+    public static void MostrarArray(int[] array) {
+        for (int i = 0; i < array.length; i++)
+            System.out.print("[" + array[i] + "] ");
+
+        System.out.println("");
+
+    }
+
+    public static void ContarRepeticiones(int[] array) {
+        for (int j = 0; j < 11; j++) {
+            int c = 0;
+            for (int i = 0; i < array.length; i++) {
+                if (array[i] == j) {
+                    c++;
+
+                }
+
+            }
+            if (c > 0)
+                System.out.println("El numero " + j + " se repite " + c + " veces.");
         }
     }
 }
