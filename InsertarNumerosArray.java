a52811b12fcce095d6baaf54e140b77f246ec4f2
jhon <a_jhondeivis.ayala.braithwite@iespablopicasso.es>
2022-11-11 11:04:20 +0100

    cambio el tamaño del array a 30

--- InsertarNumerosArray.java
+++ InsertarNumerosArray.java
@@ -1,16 +1,20 @@
 public class InsertarNumerosArray {
     public static void main(String[] args) {
-        int[] array = new int[20];
-        InsertarNumeros(array);
+        int[] array = InsertarNumeros();
+
         MostrarArray(array);
     }
 
-    public static void InsertarNumeros(int[] array) {
+    public static int[] InsertarNumeros() {
+        int[] array = new int[30];
         for (int i = 0; i < array.length; i++) {
-            array[i] = (int) (Math.random() * (10 + 1));
+            array[i] = (int) (Math.random() * 10 + 1);
+
         }
+        return array;
     }
-    public static void MostrarArray(int [] array) {
+
+    public static void MostrarArray(int[] array) {
         for (int i = 0; i < array.length; i++)
             System.out.print("[" + array[i] + "] ");
     }
